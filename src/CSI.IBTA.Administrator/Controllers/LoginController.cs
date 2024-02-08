98fc6d5c3e43c8a59a1df3b5013eba475d2f3113
VidasBui <107630990+VidasBui@users.noreply.github.com>
2024-02-08 16:27:07 +0200

    create login UI for admin portal

--- src/CSI.IBTA.Administrator/Controllers/LoginController.cs
+++ src/CSI.IBTA.Administrator/Controllers/LoginController.cs
@@ -4,21 +4,16 @@ using System.Diagnostics;
 
 namespace CSI.IBTA.Administrator.Controllers
 {
-    public class HomeController : Controller
+    public class LoginController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<LoginController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public LoginController(ILogger<LoginController> logger)
         {
             _logger = logger;
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
