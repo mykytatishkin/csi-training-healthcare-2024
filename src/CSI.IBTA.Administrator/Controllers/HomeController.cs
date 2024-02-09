6bd483070ff6b61f6ef7f09f09346ef85883b0ec
VidasBui <107630990+VidasBui@users.noreply.github.com>
2024-02-09 21:06:51 +0200

    Implement login/logout functionality

---
+++ src/CSI.IBTA.Administrator/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSI.IBTA.Administrator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/CSI.IBTA.Administrator/Controllers/LoginController.cs
+++ src/CSI.IBTA.Administrator/Controllers/LoginController.cs
@@ -1,22 +1,84 @@
 using CSI.IBTA.Administrator.Models;
+using CSI.IBTA.Shared;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace CSI.IBTA.Administrator.Controllers
 {
     public class LoginController : Controller
     {
         private readonly ILogger<LoginController> _logger;
+        private readonly HttpClient _httpClient;
 
-        public LoginController(ILogger<LoginController> logger)
+        public LoginController(ILogger<LoginController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _httpClient = new HttpClient();
+            var authServiceApiUrl = configuration.GetValue<string>("AuthServiceApiUrl");
+            if (string.IsNullOrEmpty(authServiceApiUrl))
+            {
+                _logger.LogError("AuthServiceApiUrl is missing in appsettings.json");
+                throw new InvalidOperationException("AuthServiceApiUrl is missing in appsettings.json");
+            }
+            else
+            {
+                Console.WriteLine("skdksd");
+            }
+            _httpClient.BaseAddress = new Uri(authServiceApiUrl);
         }
 
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            var apiEndpoint = "v1/Auth";
+            var dto = new LoginRequest(model.Username, model.Password);
+            var jsonBody = JsonConvert.SerializeObject(dto);
+
+            var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(apiEndpoint, content);
+            if (response.IsSuccessStatusCode)
+            {
+                var token = await response.Content.ReadAsStringAsync();
+
+                var cookieOptions = new CookieOptions
+                {
+                    HttpOnly = true
+                };
+                Response.Cookies.Append("jwtToken", token, cookieOptions);
+
+                return RedirectToAction("Index", "Home");
+            }
+            else if (response.StatusCode == HttpStatusCode.InternalServerError)
+            {
+                ModelState.AddModelError("", "Server error");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid username or password");
+            }
+            return View("Index", model);
+        }
+
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("jwtToken");
+            return RedirectToAction("Index");
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
