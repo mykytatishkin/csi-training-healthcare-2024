3d0f1d691f29272ffe54a4bbdbcf88ced9a9a39c
GytMas <86054345+GytisMas@users.noreply.github.com>
2024-02-19 16:03:32 +0200

    Employer filtering in UI

--- src/CSI.IBTA.Administrator/DependencyInjection.cs
+++ src/CSI.IBTA.Administrator/DependencyInjection.cs
@@ -20,6 +20,7 @@ namespace CSI.IBTA.Administrator
             services.AddHttpContextAccessor();
             services.AddScoped<IJwtTokenService, JwtTokenService>();
             services.AddHttpClient<IAuthClient, AuthClient>();
+            services.AddHttpClient<IUserServiceClient, UserServiceClient>();
             return services;
         }
     }
---
+++ src/CSI.IBTA.Administrator/Interfaces/IUserServiceClient.cs
@@ -0,0 +1,11 @@
+﻿using CSI.IBTA.Shared.DTOs.Login;
+using CSI.IBTA.Shared.Entities;
+using CSI.IBTA.Shared.Types;
+
+namespace CSI.IBTA.Administrator.Interfaces
+{
+    public interface IUserServiceClient
+    {
+        Task<List<Employer>?> GetEmployers(string token);
+    }
+}
