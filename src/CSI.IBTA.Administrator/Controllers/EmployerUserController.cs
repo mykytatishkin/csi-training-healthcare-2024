1361a4627e34247eaa72d86ca00f6d421e0eb715
devJUKI <49684752+devJUKI@users.noreply.github.com>
2024-02-20 13:56:53 +0200

    Minimal UI implementation

---
+++ src/CSI.IBTA.Administrator/Controllers/EmployerUserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSI.IBTA.Administrator.Controllers
+{
+    public class EmployerUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            ModelState.AddModelError("", "Test error");
+            return View("Index");
+
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ src/CSI.IBTA.Administrator/Models/CreateEmployerUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSI.IBTA.Administrator.Models
+{
+    public class CreateEmployerUserViewModel
+    {
+        [Required]
+        public string Firstname { get; set; } = null!;
+        [Required]
+        public string Lastname { get; set; } = null!;
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Username { get; set; } = null!;
+        [Required]
+        [MinLength(6)]
+        [MaxLength(20)]
+        public string Password { get; set; } = null!;
+    }
+}
