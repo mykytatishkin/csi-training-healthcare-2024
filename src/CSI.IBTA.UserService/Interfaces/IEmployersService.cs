704e230192f389e3e8b09700a7fc87b725d4fc1c
GytMas <86054345+GytisMas@users.noreply.github.com>
2024-02-22 18:05:09 +0200

    Employer settings management in user service API

--- src/CSI.IBTA.UserService/Interfaces/IEmployersService.cs
+++ src/CSI.IBTA.UserService/Interfaces/IEmployersService.cs
@@ -9,5 +9,8 @@ namespace CSI.IBTA.UserService.Interfaces
         public Task<GenericResponse<EmployerDto>> CreateEmployer(CreateEmployerDto dto);
         public Task<GenericResponse<EmployerDto>> UpdateEmployer(int employerId, UpdateEmployerDto dto);
         public Task<GenericResponse<bool>> DeleteEmployer(int employerId);
+        public Task<GenericResponse<SettingsDto[]>> GetAllEmployerSettings(int employerId);
+        public Task<GenericResponse<bool?>> GetEmployerSettingValue(int employerId, string condition);
+        public Task<GenericResponse<SettingsDto[]>> UpdateEmployerSettings(int employerId, SettingsDto[] SettingsDtos);
     }
 }
