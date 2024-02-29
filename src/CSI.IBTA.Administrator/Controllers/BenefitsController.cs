9d90e57b3db2265cfad42adf6d7d40a83acc3ee7
VidasBui <107630990+VidasBui@users.noreply.github.com>
2024-02-29 21:10:08 +0200

    scaffolding for package plan view

---
+++ src/CSI.IBTA.Administrator/Controllers/BenefitsController.cs
@@ -0,0 +1,32 @@
+﻿using CSI.IBTA.Administrator.Extensions;
+using CSI.IBTA.Administrator.Interfaces;
+using CSI.IBTA.Administrator.Models;
+using CSI.IBTA.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace CSI.IBTA.Administrator.Controllers
+{
+    [Route("Benefits")]
+    public class BenefitsController : Controller
+    {
+       
+        private readonly IUserServiceClient _userServiceClient;
+
+        public BenefitsController(IUserServiceClient userServiceClient)
+        {
+            _userServiceClient = userServiceClient;
+        }
+
+        [HttpGet("InsurancePackages")]
+        public IActionResult AdministrationMenu(int employerId)
+        {
+            var list = new List<EmployerPackageDto>()
+            {
+                new EmployerPackageDto(2,"ISSoft Package", "Not Initialized", true),
+                new EmployerPackageDto(3," Package", $"Initialized on {employerId}", false)
+            };
+            return PartialView("_EmployerPackagesMenu", list);
+        }
+    }
+}
---
+++ src/CSI.IBTA.Shared/DTOs/EmployerPackageDTOs.cs
@@ -0,0 +1,6 @@
+﻿
+
+namespace CSI.IBTA.Shared.DTOs
+{
+    public record EmployerPackageDto(int Id, string Name, string Status, bool CanBeModified);
+}
