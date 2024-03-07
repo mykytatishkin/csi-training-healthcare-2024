1ad6b6f80cc11286057b4599e56fbdb5f37f4809
VidasBui <107630990+VidasBui@users.noreply.github.com>
2024-03-07 17:36:14 +0200

    modify Claim to have Status as an enum

---
+++ src/CSI.IBTA.DB.Migrations/Migrations/BenefitsManagement/20240307152700_ClaimStatusEnum.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSI.IBTA.DB.Migrations.Migrations.BenefitsManagement
+{
+    /// <inheritdoc />
+    public partial class ClaimStatusEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Claim",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Claim",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
