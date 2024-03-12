88d7392eb8afbb5f1bf57ebdcc962e21fb0d34e7
VidasBui <107630990+VidasBui@users.noreply.github.com>
2024-03-12 14:48:48 +0200

    modify dates to use american format

---
+++ src/CSI.IBTA.Shared/Utils/Extensions/DateExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace CSI.IBTA.Shared.Utils.Extensions
+{
+    public static class DateExtensions
+    {
+        public static string ToAmericanDateOnlyFormat(this DateOnly date)
+        {
+            var cultureInfo = new CultureInfo("en-US");
+            return date.ToString("dd/MM/yyyy", cultureInfo);
+        }
+
+        public static string ToAmericanDateOnlyFormat(this DateTime date)
+        {
+            var cultureInfo = new CultureInfo("en-US");
+            return date.ToString("dd/MM/yyyy", cultureInfo);
+        }
+
+        public static string? ToAmericanDateOnlyFormat(this DateOnly? date)
+        {
+            if (date == null) return null;
+            var d = (DateOnly) date;
+            return d.ToAmericanDateOnlyFormat();
+        }
+    }
+}
