44587ea2d04c1d4502c50a011a9f86eb3112f241
devJUKI <49684752+devJUKI@users.noreply.github.com>
2024-03-19 15:34:54 +0200

    IBTA-60. Payments are done on last day of interval
    Also every payment is the same size, doesn't matter if last interval was shorter

--- src/CSI.IBTA.ProcessingService/Services/DefaultScopedProcessingService.cs
+++ src/CSI.IBTA.ProcessingService/Services/DefaultScopedProcessingService.cs
@@ -1,8 +1,8 @@
-﻿using CSI.IBTA.DataLayer.Interfaces;
+﻿using CSI.IBTA.ProcessingService.Interfaces;
 using CSI.IBTA.ProcessingService.Constants;
-using CSI.IBTA.ProcessingService.Interfaces;
-using CSI.IBTA.Shared.Entities;
+using CSI.IBTA.DataLayer.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using CSI.IBTA.Shared.Entities;
 
 namespace CSI.IBTA.ProcessingService.Services
 {
@@ -19,10 +19,12 @@ namespace CSI.IBTA.ProcessingService.Services
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
 
+                var currentDate = DateTime.UtcNow.Date;
+
                 var enrollments = await _benefitsUnitOfWork.Enrollments.GetSet()
                     .Include(e => e.Plan.Package)
                     .Where(e => e.Plan.Package.Initialized != null)
-                    .Where(e => e.Plan.Package.PlanStart <= DateTime.UtcNow && e.Plan.Package.PlanEnd >= DateTime.UtcNow)
+                    .Where(e => e.Plan.Package.PlanStart <= currentDate && e.Plan.Package.PlanEnd >= currentDate)
                     .ToListAsync();
 
                 foreach (Enrollment enrollment in enrollments)
@@ -38,29 +40,18 @@ namespace CSI.IBTA.ProcessingService.Services
                     DateTime endDate = package.PlanEnd;
                     PayrollFrequency payrollFrequency = package.PayrollFrequency;
 
-                    var payrollFrequencyTimeSpan = payrollFrequency == PayrollFrequency.Weekly
-                        ? new TimeSpan(7, 0, 0, 0)
-                        : new TimeSpan(30, 0, 0, 0);
-                    
-                    TimeSpan packageDuration = endDate - startDate;
-                    int totalNumberOfPays = (int)Math.Floor(packageDuration.TotalHours / payrollFrequencyTimeSpan.TotalHours) + 1;
+                    int payrollFrequencyDays = payrollFrequency == PayrollFrequency.Weekly ? 7 : 30;
 
-                    TimeSpan timePassed = DateTime.UtcNow - startDate;
-                    int numberOfPays = (int)Math.Floor(timePassed.TotalHours / payrollFrequencyTimeSpan.TotalHours) + 1;
+                    int numberOfPays = CalculateNumberOfPayments(payrollFrequencyDays, startDate, endDate, DateTime.UtcNow);
                     int transactionCountDifference = numberOfPays - transactionCount;
 
                     decimal totalInsuranceAmount = plan.Contribution + enrollment.Election;
-                    decimal frequencyTransactionAmount = totalInsuranceAmount / (int)packageDuration.TotalDays * (int)payrollFrequencyTimeSpan.TotalDays;
-                    
+                    int totalNumberOfPays = CalculateNumberOfPayments(payrollFrequencyDays, startDate, endDate, endDate);
+                    decimal transactionAmount = totalInsuranceAmount / totalNumberOfPays;
+
                     // Handles situations if system was down and missed payments
                     for (int i = 0; i < transactionCountDifference; i++)
                     {
-                        transactionCount++;
-
-                        decimal transactionAmount = transactionCount == totalNumberOfPays
-                            ? totalInsuranceAmount - (numberOfPays - 1) * frequencyTransactionAmount
-                            : frequencyTransactionAmount;
-
                         var transaction = new Transaction
                         {
                             Amount = transactionAmount,
@@ -77,6 +68,28 @@ namespace CSI.IBTA.ProcessingService.Services
 
                 await Task.Delay(WorkerConfigConstants.CHECK_INTERVAL_MS, stoppingToken);
             }
+        }
+
+        private int CalculateNumberOfPayments(int payrollFrequencyDays, DateTime startDate, DateTime endDate, DateTime currentDate)
+        {
+            TimeSpan timeSpanToNow = currentDate - startDate;
+            int numberOfDaysToNow = (int)timeSpanToNow.TotalDays;
+            int numberOfPaymentsToNow = numberOfDaysToNow / payrollFrequencyDays;
+
+            TimeSpan timeSpanToEnd = endDate - startDate;
+            int numberOfDaysToEnd = (int)timeSpanToEnd.TotalDays;
+            int fullNumberOfPaymentsToEnd = numberOfDaysToEnd / payrollFrequencyDays;
+
+            bool hasIntervalRemainder = (int)timeSpanToEnd.TotalDays % payrollFrequencyDays == 0;
+
+            if (!hasIntervalRemainder &&
+                numberOfPaymentsToNow == fullNumberOfPaymentsToEnd &&
+                currentDate.Date == endDate.Date)
+            {
+                numberOfPaymentsToNow++;
+            }
+
+            return numberOfPaymentsToNow;
         }
     }
 }
