8a5df60c987f8a168f512d8162d42a6c0b61ec91
devJUKI <49684752+devJUKI@users.noreply.github.com>
2024-03-22 17:31:10 +0200

    IBTA-52. Frontend completed

---
+++ src/CSI.IBTA.Employer/Controllers/EmployeesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSI.IBTA.Employer.Controllers
+{
+    [Route("{controller}")]
+    public class EmployeesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(int employerId)
+        {
+            return PartialView("_Employees");
+        }
+    }
+}
---
+++ src/CSI.IBTA.Employer/wwwroot/Scripts/shared.js
@@ -0,0 +1,12 @@
+﻿window.onload = function () {
+    showEmployeeList(1);
+}
+
+function showEmployeeList(employerId) {
+    function onSuccess(data) {
+        document.getElementById('partial-screen').innerHTML = data;
+    }
+
+    route = '/Employees?employerId=' + employerId;
+    fetchRoute(route, onSuccess, null);
+}
