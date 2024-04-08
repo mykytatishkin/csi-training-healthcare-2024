e45251b441cedb894d9bcdc324add9b9e41aac67
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2024-04-08 05:57:34 +0300

    totally clear

--- src/CSI.IBTA.Consumer/Endpoints/EmployerEndpoints.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CSI.IBTA.Consumer.Endpoints
-{
-    public static class EmployerEndpoints
-    {
-        public const string Employer = "v1/Employer";
-        public const string GetEmployerByAccountId = "v1/Employer/GetByAccountId";
-    }
-}
