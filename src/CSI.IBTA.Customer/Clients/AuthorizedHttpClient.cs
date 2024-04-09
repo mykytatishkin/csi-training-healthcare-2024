9759ed773bab52b9e40a55be7e806c2631858667
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2024-04-09 17:27:23 +0300

    consumer -> customer

---
+++ src/CSI.IBTA.Customer/Clients/AuthorizedHttpClient.cs
@@ -0,0 +1,51 @@
+﻿using CSI.IBTA.Customer.Interfaces;
+using System.Net.Http.Headers;
+
+namespace CSI.IBTA.Customer.Clients
+{
+    internal class AuthorizedHttpClient : HttpClient
+    {
+        private readonly IJwtTokenService _jwtTokenService;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<AuthorizedHttpClient> _logger;
+
+        public AuthorizedHttpClient(
+            IJwtTokenService jwtTokenService,
+            IConfiguration configuration,
+            ILogger<AuthorizedHttpClient> logger)
+        {
+            _jwtTokenService = jwtTokenService;
+            _configuration = configuration;
+            _logger = logger;
+
+            string token = _jwtTokenService.GetCachedToken() ?? string.Empty;
+            DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        public string GetBaseAddress(string urlConfigurationString)
+        {
+            var apiUrl = _configuration.GetValue<string>(urlConfigurationString);
+
+            if (string.IsNullOrEmpty(apiUrl))
+            {
+                _logger.LogError("API URL \"{urlConfigurationString}\" is missing in appsettings.json", urlConfigurationString);
+                throw new InvalidOperationException($"API URL \"{urlConfigurationString}\" is missing in appsettings.json");
+            }
+
+            return apiUrl;
+        }
+
+        public void SetBaseAddress(string urlConfigurationString)
+        {
+            var apiUrl = _configuration.GetValue<string>(urlConfigurationString);
+
+            if (string.IsNullOrEmpty(apiUrl))
+            {
+                _logger.LogError("API URL \"{urlConfigurationString}\" is missing in appsettings.json", urlConfigurationString);
+                throw new InvalidOperationException($"API URL \"{urlConfigurationString}\" is missing in appsettings.json");
+            }
+
+            BaseAddress = new Uri(apiUrl);
+        }
+    }
+}
--- src/CSI.IBTA.Customer/Controllers/HomeController.cs
+++ src/CSI.IBTA.Customer/Controllers/HomeController.cs
@@ -1,32 +1,15 @@
-using CSI.IBTA.Customer.Models;
+using CSI.IBTA.Customer.Filters;
+using CSI.IBTA.Customer.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace CSI.IBTA.Customer.Controllers
 {
+    [TypeFilter(typeof(AuthenticationFilter))]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
