c57be4d40eb057c910d8cc88f98322cb126b3a92
GytMas <86054345+GytisMas@users.noreply.github.com>
2024-04-10 17:28:00 +0300

    Front end claims screen

--- src/CSI.IBTA.Customer/Controllers/ClaimsController.cs
+++ src/CSI.IBTA.Customer/Controllers/ClaimsController.cs
@@ -1,13 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CSI.IBTA.Customer.Models;
+using CSI.IBTA.Shared.DTOs;
+using CSI.IBTA.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CSI.IBTA.Customer.Controllers
 {
     [Route("[controller]")]
     public class ClaimsController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(
+            int employerId,
+            string? firstnameFilter,
+            string? lastnameFilter,
+            string? ssnFilter,
+            string? currentFirstnameFilter,
+            string? currentLastnameFilter,
+            string? currentSsnFilter,
+            int? pageNumber)
         {
-            return PartialView("_Claims");
+            var claims = new List<ClaimDto>();
+            claims.Add(new ClaimDto(1, 3, 4, 5, "LT202201185181", DateOnly.FromDateTime(DateTime.UtcNow), "PLANN", "Medical", 100, ClaimStatus.Pending, null));
+            claims.Add(new ClaimDto(1, 3, 4, 5, "LT202201185121", DateOnly.FromDateTime(DateTime.UtcNow), "PLANN", "Mental", 75, ClaimStatus.Approved, null));
+            claims.Add(new ClaimDto(1, 3, 4, 5, "LT202201185141", DateOnly.FromDateTime(DateTime.UtcNow), "PLANN", "Dental", 50, ClaimStatus.Denied, null));
+
+            var viewModel = new ClaimsSearchViewModel
+            {
+                Claims = claims,
+                EmployerId = employerId,
+                EmployerClaimFilling = true,
+                Page = pageNumber ?? 1,
+                TotalCount = 6,
+                TotalPages = 2,
+            };
+
+            return PartialView("_Claims", viewModel);
         }
     }
 }
