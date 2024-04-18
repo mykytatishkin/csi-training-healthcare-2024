440b7c6685c14dda10faff988d79ddf98f677c7f
VidasBui <107630990+VidasBui@users.noreply.github.com>
2024-04-18 11:13:09 +0300

    secure encrypt controller

---
+++ src/CSI.IBTA.UserService/Authorization/Extensions/EmployeeOwnedResourceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using CSI.IBTA.Shared.Authorization.Types;
+
+namespace CSI.IBTA.UserService.Authorization.Extensions
+{
+    public static class EmployeeOwnedResourceExtensions
+    {
+        public static EmployeeOwnedResource GetResource(this (int employeeId, int employerId) data) 
+        {
+            return new EmployeeOwnedResource()
+            {
+                EmployeeId = data.employeeId,
+                EmployerId = data.employerId,
+            };
+        }
+    }
+}
--- src/CSI.IBTA.UserService/Controllers/EncryptController.cs
+++ src/CSI.IBTA.UserService/Controllers/EncryptController.cs
@@ -1,4 +1,7 @@
-﻿using CSI.IBTA.Shared.Entities;
+﻿using CSI.IBTA.Shared.Authorization.Types;
+using CSI.IBTA.Shared.Entities;
+using CSI.IBTA.UserService.Authorization.Constants;
+using CSI.IBTA.UserService.Authorization.Extensions;
 using CSI.IBTA.UserService.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,16 +13,21 @@ namespace CSI.IBTA.UserService.Controllers
     public class EncryptController : Controller
     {
         private readonly IEncodingService _encodingService;
+        private readonly IAuthorizationService _authorizationService;
 
-        public EncryptController(IEncodingService encodingService)
+        public EncryptController(IEncodingService encodingService, IAuthorizationService authService)
         {
             _encodingService = encodingService;
+            _authorizationService = authService;
         }
 
         [HttpGet("Employer/{employerId}/Employee/{employeeId}")]
         [Authorize(Roles = $"{nameof(Role.EmployerAdmin)}, {nameof(Role.Employee)}")]
         public async Task<IActionResult> EncodeEmployerEmployee(int employerId, int employeeId)
         {
+            var authRes = await _authorizationService.AuthorizeAsync(User, (employeeId, employerId).GetResource(), PolicyConstants.EmployeeOwner);
+            if (!authRes.Succeeded) return Forbid();
+
             var response = await _encodingService.GetEncodedEmployerEmployee(employerId, employeeId);
 
             if (response.Error != null)
