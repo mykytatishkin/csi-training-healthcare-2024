be6ab6576a09fe75c9db2f4e8467d0233f3bf5dd
devJUKI <49684752+devJUKI@users.noreply.github.com>
2024-04-19 02:11:52 +0300

    IBTA-83. Fixed Benefits Management Context snapshot

--- src/CSI.IBTA.DB.Migrations/Migrations/BenefitsManagement/BenefitsManagementContextModelSnapshot.cs
+++ src/CSI.IBTA.DB.Migrations/Migrations/BenefitsManagement/BenefitsManagementContextModelSnapshot.cs
@@ -36,7 +36,7 @@ namespace CSI.IBTA.DB.Migrations.Migrations.BenefitsManagement
                     b.Property<string>("ClaimNumber")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
-
+                    
                     b.Property<DateOnly>("DateOfService")
                         .HasColumnType("date");
 
@@ -46,10 +46,6 @@ namespace CSI.IBTA.DB.Migrations.Migrations.BenefitsManagement
 
                     b.Property<int>("EnrollmentId")
                         .HasColumnType("int");
-
-                    b.Property<string>("Receipt")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("RejectionReason")
                         .HasColumnType("nvarchar(max)");
